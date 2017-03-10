06f1a68f121f9da3a4a2f30db363420d109a584f
Aleksandra <uxaleksandra@gmail.com>
2017-03-10 13:59:07 +0100

    task

---
+++ js/scripts.js
@@ -0,0 +1,122 @@
+$(function() {
+
+	function randomString() {
+	    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+	    var str = '';
+	    var i = 0;
+	    for (i = 0; i < 10; i++) {
+	        str += chars[Math.floor(Math.random() * chars.length)];
+	    }
+	    return str;
+	}
+
+	function Column(name) {
+	    var self = this; 
+	    this.id = randomString();
+	    this.name = name;
+	    this.$element = createColumn();
+		
+		function createColumn() {
+			var $column = $('<div>').addClass('column');
+			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+			var $columnCardList = $('<ul>').addClass('column-list');
+			var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+			var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj kartę');
+
+			$columnDelete.click(function() {
+			   self.removeColumn();
+			});
+			$columnAddCard.click(function(event) {
+			  self.addCard(new Card(prompt("Wpisz nazwę karty")));
+			});
+
+
+			$column.append($columnTitle)
+			  .append($columnDelete)
+			  .append($columnAddCard)
+			  .append($columnCardList);
+
+			  return $column;
+		}
+	}
+	
+	Column.prototype = {
+	    addCard: function(card) {
+	      this.$element.children('ul').append(card.$element);
+	    },
+	    removeColumn: function() {
+	     this.$element.remove();
+	    }
+	};
+  	
+
+	function Card(description) {
+		var self = this;
+		this.id = randomString();
+		this.description = description;
+		this.$element = createCard(); 
+
+		function createCard() {
+		    var $card = $('<li>').addClass('card');
+		    var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+		    var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+		    $cardDelete.click(function(){
+		        self.removeCard();
+		    });
+
+		    $card.append($cardDelete)
+		        .append($cardDescription);
+
+	      	return $card;
+	    }
+	}
+
+	Card.prototype = {
+	    removeCard: function() {
+		   	this.$element.remove();
+	   	}
+	};
+	
+
+	function initSortable() {
+	    $('.column-card-list').sortable({
+	      connectWith: '.column-card-list',
+	      placeholder: 'card-placeholder'
+	    }).disableSelection();
+	
+	}
+
+	var board = {
+		name: 'Tablica Kanban',
+		addColumn: function(column) {
+		    this.$element.append(column.$element);
+		    initSortable();
+		},
+		$element: $('#board .column-container')
+
+	};
+
+	$('.create-column')
+			  .click(function(){
+			  var name = prompt('Wpisz nazwę kolumny');
+			  var column = new Column(name);
+		      board.addColumn(column);
+	});		
+
+
+	var todoColumn = new Column('Do zrobienia');
+	var doingColumn = new Column('W trakcie');
+	var doneColumn = new Column('Skończone');
+
+	board.addColumn(todoColumn);
+	board.addColumn(doingColumn);
+	board.addColumn(doneColumn);
+
+	var card1 = new Card('Nowe zadanie');
+	var card2 = new Card('Stworzyc tablice kanban');
+
+	todoColumn.addCard(card1);
+	doingColumn.addCard(card2);
+
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+﻿OOP
+Zadanie: Trello
